ece5001b9643b9ec9e0a339c546e2f443119ed72
Lee <ycp998@naver.com>
2022-03-26 23:32:12 +0900

    build : boilerplate init

--- src/javascripts/global/api/index.ts
+++
@@ -1,9 +0,0 @@
-export const DOMAIN = 'http://localhost:8080/';
-// 과제가 아닌 production code 라면 dotenv 사용
-
-export const API = {
-  GET: {
-    CARLIST: `${DOMAIN}carClasses`,
-    CAR: `${DOMAIN}carClass`,
-  },
-};
